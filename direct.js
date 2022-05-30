5e8f6d8e3e43d971d31ba574763bedf6d5c8ecfe
jakarialucky <34542947+jakarialucky@users.noreply.github.com>
2022-05-30 11:32:50 +0800

    Create direct.js

---
+++ direct.js
@@ -0,0 +1,12 @@
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://flickerbridge.com/jvc7j1tkig?key=c66d0e08f8c29db24bae4694b4ff0db5', '_blank');
+	});
+//]]>
