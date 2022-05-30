749cce07db7cf8a47b3094fb9b2812dc28765dcc
jakarialucky <34542947+jakarialucky@users.noreply.github.com>
2022-05-30 11:17:44 +0800

    Create ads_320_50.js

---
+++ ads_320_50.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '2d37e6d07504ef306df285fbfc54ea4e',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://flickerbridge.com/2d37e6d07504ef306df285fbfc54ea4e/invoke.js"></scr' + 'ipt>');
