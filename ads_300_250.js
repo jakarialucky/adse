ee28baabd83727fedc5c98eb7c2b94b733d7cbab
jakarialucky <34542947+jakarialucky@users.noreply.github.com>
2023-07-13 11:12:30 +0800

    Update ads_300_250.js

--- ads_300_250.js
+++ ads_300_250.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'a2fa1c021faaa64dc52545dcd0d45f38',
+		'key' : '06395aa5e68b179c6493e25e4df12c50',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://flickerbridge.com/a2fa1c021faaa64dc52545dcd0d45f38/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://flickerbridge.com/06395aa5e68b179c6493e25e4df12c50/invoke.js"></scr' + 'ipt>');
