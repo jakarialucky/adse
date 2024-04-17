b2f033dfc6f00863fe23f8546b2672131fc34728
jakarialucky <34542947+jakarialucky@users.noreply.github.com>
2024-04-17 14:52:21 +0800

    Update iklan_otomatis_blogspot.js

--- iklan_otomatis_blogspot.js
+++ iklan_otomatis_blogspot.js
@@ -8,7 +8,7 @@ function masukkanIklan(iklanId, scriptCode) {
 }
 
 // Memasukkan skrip iklan ke elemen-elemen HTML
-masukkanIklan('iklan_banner', '<script type="text/javascript"> // Script iklan_banner </script>');
+masukkanIklan('iklan_banner', '<script type="text/javascript">atOptions={'key':'a2fa1c021faaa64dc52545dcd0d45f38','format':'iframe','height':250,'width':300,'params':{}};</script><script type="text/javascript" src="//affordedseasick.com/a2fa1c021faaa64dc52545dcd0d45f38/invoke.js"></script>');
 masukkanIklan('iklan_sidebar', '<script type="text/javascript"> // Script iklan_sidebar </script>');
 masukkanIklan('iklan_atas_artikel', '<script type="text/javascript"> // Script iklan_atas_artikel </script>');
 masukkanIklan('iklan_tengah_artikel', '<script type="text/javascript"> // Script iklan_tengah_artikel </script>');
